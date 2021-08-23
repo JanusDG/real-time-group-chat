c0a9a05be02793f46667d5342574bc831d496afe
JanusDG <d.bilusyak@gmail.com>
2021-08-24 01:45:53 +0300

    added simple server

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/JanusDG/real-time-group-chat
+
+go 1.16
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/spf13/viper v1.8.1
+)
--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
-import "github.com/JanusDG/real-time-group-chat/server"
+import ("fmt"
+		"os"
+		"github.com/JanusDG/real-time-group-chat/server"
+		"github.com/JanusDG/real-time-group-chat/config"
+)
 
 func main() {
-	//cfg := NewConfig()
+	server.Dudka()
 
-	serv := server.NewServer()
+	var config = config.GetConf()
 
-	serv.RunSession()
+	// serv := server.NewServer(config.Server.Port) 
+
+	serv := new(server.Server)
+	serv.Init(config.Server.Port)
+
+
+	if (config.DEBUG_ON){ fmt.Println("Created server instance on port:",serv.Port) }
+	if (config.DEBUG_ON){ fmt.Println(os.Getenv("DEBUG_ON")) }
+	
+
+
+
+	// server.RunServer()
+	serv.RunServer()
+
+	
+
 }
